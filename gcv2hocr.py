6f1dfa347eff83efe1ca0ee0371057f545a97f0c
Konstantin Baierer <unixprog@gmail.com>
2016-09-12 16:19:03 +0200

    No leading newline

--- gcv2hocr.py
+++ gcv2hocr.py
@@ -8,8 +8,7 @@ from string import Template
 class GCVAnnotation:
 
     templates = {
-        'ocr_page': Template("""
-<?xml version="1.0" encoding="UTF-8"?>
+        'ocr_page': Template("""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="$lang" lang="$lang">
   <head>
