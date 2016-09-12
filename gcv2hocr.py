98e5fd91f339411c28ab99926995f1e07af27db8
Konstantin Baierer <unixprog@gmail.com>
2016-09-12 16:12:47 +0200

    Ported to python
    dinosauria123/gcv2hocr#2
    UB-Mannheim/ocr-fileformat#28

---
+++ gcv2hocr.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import argparse
+from string import Template
+
+class GCVAnnotation:
+
+    templates = {
+        'ocr_page': Template("""
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="$lang" lang="$lang">
+  <head>
+    <title>$title</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name='ocr-system' content='gcv2hocr.py' />
+    <meta name='ocr-capabilities' content='ocr_page ocr_line ocrx_word ocrp_lang'/>
+  </head>
+  <body>
+    <div class='ocr_page' lang='$lang' title='bbox $x0 $y0 $x1 $y1'>$content
+    </div>
+  </body>
+</html>
+    """),
+        'ocr_line': Template("""
+    <span class='ocr_line' id='$htmlid' title='bbox $x0 $y0 $x1 $y1'>$content
+    </span>"""),
+        'ocrx_word': Template("""
+        <span class='ocrx_word' id='$htmlid' title='bbox $x0 $y0 $x1 $y1'>$content</span>""")
+    }
+
+    def __init__(self,
+                 htmlid=None,
+                 ocr_class=None,
+                 lang='unknown',
+                 content=[],
+                 box=None,
+                 title=''):
+        self.title = title
+        self.htmlid = htmlid
+        self.lang = lang
+        self.ocr_class = ocr_class
+        self.content = content
+        self.x0 = box[0]['x']
+        self.y0 = box[0]['y']
+        self.x1 = box[2]['x']
+        self.y1 = box[2]['y']
+
+    def maximize_bbox(self):
+        self.x0 = min([w.x0 for w in self.content])
+        self.y0 = min([w.y0 for w in self.content])
+        self.x1 = max([w.x1 for w in self.content])
+        self.y1 = max([w.y1 for w in self.content])
+
+    def __repr__(self):
+        return "<%s [%s %s %s %s]>%s</%s>" % (self.ocr_class, self.x0, self.y0,
+                                              self.x1, self.y1, self.content,
+                                              self.ocr_class)
+    def render(self):
+        if type(self.content) == type([]):
+            content = "".join(map(lambda x: x.render(), self.content))
+        else:
+            content = self.content
+        return self.__class__.templates[self.ocr_class].substitute(self.__dict__, content=content)
+
+def fromResponse(resp, baseline_tolerance=2, lang='unknown', title=''):
+    last_baseline = -100
+    page = None
+    curline = None
+    for anno_idx, anno_json in enumerate(resp['textAnnotations']):
+        box = anno_json['boundingPoly']['vertices']
+        if anno_idx == 0:
+            page = GCVAnnotation(
+                ocr_class='ocr_page',
+                lang=lang,
+                title=title,
+                htmlid='page_0',
+                content=[],
+                box=box)
+            continue
+        word = GCVAnnotation(ocr_class='ocrx_word', content=anno_json['description'], box=box)
+        if word.y1-abs(last_baseline) > baseline_tolerance:
+            curline = GCVAnnotation(
+                ocr_class='ocr_line',
+                htmlid="line_%d" % (len(page.content)),
+                content=[],
+                box=box)
+            page.content.append(curline)
+            last_baseline = word.y1
+        word.htmlid="word_%d_%d" % (len(page.content) - 1, len(curline.content))
+        curline.content.append(word)
+    for line in page.content:
+        line.maximize_bbox()
+    page.maximize_bbox()
+    return page
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('gcv_file', help='GCV JSON file, "-" for STDIN')
+    parser.add_argument(
+        "--baseline-tolerance",
+        "-T",
+        help="Y Tolerance to recognize same line. Default: 2",
+        metavar="INT",
+        type=int,
+        default=2)
+    parser.add_argument(
+        "--title",
+        "-t",
+        help="Document title")
+    parser.add_argument(
+        "--lang",
+        "-L",
+        default='unknown',
+        help="Language")
+    parser.add_argument(
+        "--width",
+        "-W",
+        help="Image width. Automatically detected unless specified")
+    parser.add_argument(
+        "--height",
+        "-H",
+        help="Image height. Automatically detected unless specified")
+    args = parser.parse_args()
+
+    instream = sys.stdin if args.gcv_file is '-' else open(args.gcv_file, 'r')
+    resp = json.load(instream)['responses'][0]
+    page = fromResponse(
+        resp,
+        title=args.title,
+        lang=args.lang,
+        baseline_tolerance=args.baseline_tolerance)
+    if str == bytes:
+        print(page.render().encode('utf-8'))
+    else:
+        print(page.render())
