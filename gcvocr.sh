ad62622482128899db3d989029ab018f15175d58
ENDO Michiaki <dinosaur@db3.so-net.ne.jp>
2017-10-23 16:45:03 +0900

    Added languageHints option

--- gcvocr.sh
+++ gcvocr.sh
@@ -2,9 +2,7 @@
 #cd ~
 echo '{"requests":[{"image":{"content":"' > ./temp.json
 openssl base64 -in $1 | cat >> ./temp.json
-echo '"},"features":[{
-          "type":"TEXT_DETECTION",
-          "maxResults":2048
-}]}]}' >> ./temp.json
+echo '"},
+"features":{"type":"TEXT_DETECTION","maxResults":2048},"imageContext":{"languageHints":"ja"}}]}' >> ./temp.json
 curl -k -s -H "Content-Type: application/json" https://vision.googleapis.com/v1/images:annotate?key=$2 --data-binary @./temp.json > "$1.json"
 rm ./temp.json
